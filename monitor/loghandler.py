00cd6cacb3c39bc46d81246b32e324709b4c62d9
Syeda Persia Aziz <syaziz@microsoft.com>
2019-09-13 17:08:05 +0000

    fix

--- monitor/loghandler.py
+++ monitor/loghandler.py
@@ -1,8 +1,9 @@
 import logging
+import sys
 from azure_storage_logging.handlers import QueueStorageHandler
 
-logger = logging.getHandler("sapmonitortest")
-handler = (account_name=None, account_key=None, protocol='https', queue='logs', message_ttl=None, visibility_timeout=None, base64_encoding=False, is_emulated=False)
+logger = logging.getLogger("sapmonitortest")
+handler = QueueStorageHandler(account_name=sys.argv[1], account_key=sys.argv[2], protocol='https', queue='logs', message_ttl=None, visibility_timeout=None, base64_encoding=False, is_emulated=False)
 logger.addHandler(handler)
 logger.info("seriously?")
 logger.warning("ya ya")
